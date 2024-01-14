b49ac3a361d26557fbdb06bb73c63dccfdb0e7c2
brianhanna <bhanna1693@gmail.com>
2024-01-14 00:22:11 -0500

    deploy change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ TAILWIND_RELEASE="tailwindcss-macos-x64"
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/$TAILWIND_RELEASE
 chmod +x $TAILWIND_RELEASE
 mv $TAILWIND_RELEASE tailwindcss
-./tailwindcss -i ./../tailwind.css -o ./../web/static/styles/tailwind.css --minify
+./deploy/tailwindcss -i ./../tailwind.css -o ./../web/static/styles/tailwind.css --minify
 templ generate
 go build -o ./tmp/main ./cmd
